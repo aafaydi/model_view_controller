2dd5f9add6a385dc7f290e6580615d1fae8cfb08
Ahmad Faydi <aafaydi@hotmail.com>
2022-05-11 12:42:49 -0400

    update

---
+++ seeds/user-seeds.js
@@ -0,0 +1,40 @@
+const { User } = require('../models');
+
+
+const userData = [
+{
+username: "Cesar",
+email: "cesar@gmail.com",
+password: "testpassword1"
+},
+{
+username: "Denis",
+email: "denis@gmail.com",
+password: "testpassword2"
+},
+{
+username: "George",
+email: "George@gmail.com",
+password: "testpassword3"
+},
+{
+username: "Matthew",
+email: "matthew@gmail.com",
+password: "testpassword4"
+},
+{
+username: "Rachel",
+email: "rachel@gmail.com",
+password: "testpassword5"
+}
+];
+
+
+const seedUsers = () => User.bulkCreate(userData);
+
+
+
+
+
+
+module.exports = seedUsers;
