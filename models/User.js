af0c38f7d45b2b6b78e614bf64baabe674bec0dd
Ahmad Faydi <aafaydi@hotmail.com>
2022-06-05 23:13:12 -0400

    update

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 
